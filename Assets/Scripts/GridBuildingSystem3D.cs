56e8e0073d6a5027ff71b7bb10bdee9cff763283
Stephen Hubbard <hubbard_stephen@yahoo.com>
2022-11-01 14:55:43 -0600

    project hex grid set up

---
+++ Assets/Scripts/GridBuildingSystem3D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridBuildingSystem3D : MonoBehaviour {
+
+    public static GridBuildingSystem3D Instance { get; private set; }
+
+    public event EventHandler OnSelectedChanged;
+    public event EventHandler OnObjectPlaced;
+    [SerializeField] private GameObject hexVisualPrefab;
+    public int gridWidth = 10;
+    public int gridHeight = 10;
+    public float cellSize = 10f;
+
+    private GridSystemHex<GridObject> grid;
+
+    private void Awake() {
+        Instance = this;
+
+        grid = new GridSystemHex<GridObject>(gridWidth, gridHeight, cellSize, (GridSystemHex<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
+
+
+    }
+
+    private void Start() {
+        SpawnHexVisuals();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            // Debug.Log(grid.GetGridPosition(Mouse3D.GetMouseWorldPosition()));
+        }
+    }
+
+    private void SpawnHexVisuals() {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int z = 0; z < gridHeight; z++)
+            {
+                Instantiate(hexVisualPrefab, grid.GetWorldPosition(x, z), hexVisualPrefab.transform.rotation);
+            }
+        }
+    }
+
+    public class GridObject {
+
+        private GridSystemHex<GridObject> grid;
+
+        private GridPosition gridPosition;
+
+        public GridObject(GridSystemHex<GridObject> grid, GridPosition gridPosition) {
+            this.grid = grid;
+            this.gridPosition = gridPosition;
+        }
+
+        public override string ToString() {
+            return gridPosition.x + ", " + gridPosition.z;
+        }
+    }
+}
---
+++ Assets/Scripts/GridSystemHex.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridSystemHex<TGridObject> {
+
+    private const float HEX_VERTICAL_OFFSET_MULTIPLIER = .75f;
+
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs {
+        public int x;
+        public int z;
+    }
+
+    private int width;
+    private int height;
+    private float cellSize;
+    // private Vector3 originPosition;
+    private TGridObject[,] gridArray;
+
+    public GridSystemHex(int width, int height, float cellSize,Func<GridSystemHex<TGridObject>, GridPosition, TGridObject> createGridObject) {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        // this.originPosition = originPosition;
+
+        gridArray = new TGridObject[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridArray[x, z] = createGridObject(this, gridPosition);
+            }
+        }
+
+
+        bool showDebug = true;
+        if (showDebug) {
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++) {
+                for (int z = 0; z < gridArray.GetLength(1); z++) {
+                    debugTextArray[x, z] = UtilsClass.CreateWorldText(gridArray[x, z]?.ToString(), null, GetWorldPosition(x, z), 15, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center);
+                }
+            }
+
+            OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) => {
+                debugTextArray[eventArgs.x, eventArgs.z].text = gridArray[eventArgs.x, eventArgs.z]?.ToString();
+            };
+        }
+    }
+
+    public int GetWidth() {
+        return width;
+    }
+
+    public int GetHeight() {
+        return height;
+    }
+
+    public float GetCellSize() {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int z) {
+        return
+            new Vector3(x, 0, 0) * cellSize + 
+            new Vector3(0, 0, z) * cellSize * HEX_VERTICAL_OFFSET_MULTIPLIER + 
+            (((z % 2) == 1) ? new Vector3(1, 0, 0) * cellSize * .5f : Vector3.zero);
+
+    }
+
+    public void GetXZ(Vector3 worldPosition, out int x, out int z) {
+        x = Mathf.FloorToInt((worldPosition).x / cellSize);
+        z = Mathf.FloorToInt((worldPosition).z / cellSize);
+    }
+
+    public void SetGridObject(int x, int z, TGridObject value) {
+        if (x >= 0 && z >= 0 && x < width && z < height) {
+            gridArray[x, z] = value;
+            TriggerGridObjectChanged(x, z);
+        }
+    }
+
+    public void TriggerGridObjectChanged(int x, int z) {
+        OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, z = z });
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TGridObject value) {
+        GetXZ(worldPosition, out int x, out int z);
+        SetGridObject(x, z, value);
+    }
+
+    public TGridObject GetGridObject(int x, int z) {
+        if (x >= 0 && z >= 0 && x < width && z < height) {
+            return gridArray[x, z];
+        } else {
+            return default(TGridObject);
+        }
+    }
+
+    public Vector3 GetWorldPosition(GridPosition gridPosition)
+    {
+        return
+            new Vector3(gridPosition.x, 0, 0) * cellSize +
+            new Vector3(0, 0, gridPosition.z) * cellSize * HEX_VERTICAL_OFFSET_MULTIPLIER +
+            (((gridPosition.z % 2) == 1) ? new Vector3(1, 0, 0) * cellSize * .5f : Vector3.zero);
+    }
+
+
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        GridPosition roughXZ = new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / cellSize),
+            Mathf.RoundToInt(worldPosition.z / cellSize / HEX_VERTICAL_OFFSET_MULTIPLIER)
+        );
+
+        bool oddRow = roughXZ.z % 2 == 1;
+
+        List<GridPosition> neighbourGridPositionList = new List<GridPosition>
+        {
+            roughXZ + new GridPosition(-1, 0),
+            roughXZ + new GridPosition(+1, 0),
+
+            roughXZ + new GridPosition(0, +1),
+            roughXZ + new GridPosition(0, -1),
+
+            roughXZ + new GridPosition(oddRow ? +1 : -1, +1),
+            roughXZ + new GridPosition(oddRow ? +1 : -1, -1),
+        };
+
+        GridPosition closestGridPosition = roughXZ;
+
+        foreach (GridPosition neighbourGridPosition in neighbourGridPositionList)
+        {
+            if (Vector3.Distance(worldPosition, GetWorldPosition(neighbourGridPosition)) <
+                Vector3.Distance(worldPosition, GetWorldPosition(closestGridPosition)))
+            {
+                // Closer than the Closest
+                closestGridPosition = neighbourGridPosition;
+            }
+        }
+
+        return closestGridPosition;
+    }
+
+
+    public TGridObject GetGridObject(Vector3 worldPosition) {
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        return GetGridObject(x, z);
+    }
+
+    public Vector2Int ValidateGridPosition(Vector2Int gridPosition) {
+        return new Vector2Int(
+            Mathf.Clamp(gridPosition.x, 0, width - 1),
+            Mathf.Clamp(gridPosition.y, 0, height - 1)
+        );
+    }
+
+}
